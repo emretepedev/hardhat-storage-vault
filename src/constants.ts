897905059f44a197ee87150940cc5fea3e6bb06d
emretepedev <emretepedev@gmail.com>
2022-12-21 05:50:02 +0300

    chore: add initial files

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const PLUGIN_NAME = "hardhat-storage-check";
+export const TASK_STORAGE_CHECK = "storage-check";
+export const DEFAULT_CONFIG_PATH = "storage-check.json";
+export const DEFAULT_NO_COMPILE = false;
+export const DEFAULT_RUN_ON_COMPILE = false;
