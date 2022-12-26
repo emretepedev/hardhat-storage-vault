74b6d831093642aad65cd5f3a516300e4b2bf294
emretepedev <emretepedev@gmail.com>
2022-12-26 11:44:04 +0300

    test: update tests

--- test/project.test.ts
+++ test/project.test.ts
@@ -25,24 +25,9 @@ describe("Integration tests", function () {
       sandbox.restore();
     });
 
-    it("Should send warning when compiler disable", async function () {
-      await this.hre.run(TASK_STORAGE_CHECK, { compile: false });
-      expect(getConsoleMessage()).to.include(
-        `Warning in plugin ${PLUGIN_NAME}`
-      );
-    });
-
-    it("Should not send warning when compiler enable", async function () {
-      await this.hre.run(TASK_STORAGE_CHECK, { compile: true });
-      expect(getConsoleMessage()).to.not.include(
-        `Warning in plugin ${PLUGIN_NAME}`
-      );
-    });
-
     it("Should success when use storage-store.json", async function () {
       await this.hre.run(TASK_STORAGE_CHECK, {
         storePath: "storage-store.json",
-        compile: true,
       });
       expect(getConsoleMessage(2)).to.include(
         `Success in plugin ${PLUGIN_NAME}`
@@ -53,7 +38,6 @@ describe("Integration tests", function () {
       try {
         await this.hre.run(TASK_STORAGE_CHECK, {
           storePath: "error-storage-store.json",
-          compile: true,
         });
       } catch (error) {
         assert.instanceOf(error, HardhatPluginError);
@@ -69,10 +53,6 @@ describe("Integration tests", function () {
         this.hre.config.storageVault.check.storePath,
         "storage-store.json"
       );
-    });
-
-    it("Should add the 'compile' to the config", function () {
-      assert.equal(this.hre.config.storageVault.check.compile, false);
     });
   });
 });
