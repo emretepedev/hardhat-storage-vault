06c303a7938bf6bddfe1c19b44f9efaac313ed34
emretepedev <emretepedev@gmail.com>
2023-01-13 23:32:59 +0300

    chore: add cspell lint

---
+++ cspell.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  version: "0.2",
+
+  language: "en",
+
+  words: [],
+
+  ignoreWords: ["emre", "tepe", "emretepedev"],
+
+  ignorePaths: [".gitignore", ".git", "yarn.lock"],
+
+  allowCompoundWords: true,
+
+  dictionaries: [
+    "javascript",
+    "typescript",
+    "npm",
+    "node",
+    "filetypes",
+    "softwareTerms",
+    "dockerfile",
+    "markdown",
+    "xml",
+    "yaml",
+    "bash",
+    "powershell",
+    "cpp",
+    "c",
+    "json",
+    "jsonc",
+    "plaintext",
+    "aws",
+    "public-licenses",
+    "cryptocurrencies",
+    "companies",
+    "misc",
+    "en-gb",
+    "lorem-ipsum",
+  ],
+};
